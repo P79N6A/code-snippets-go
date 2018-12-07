1b7555cb6fbae639318a83f70ab47163a27ce87e
clip <binderclip2014@gmail.com>
2018-12-08 01:01:17 +0800

    pointer struct

---
+++ snippets/pointer_sp/pointer_sp.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Vertex 向量
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+
+	v := Vertex{1, 2}
+	p2 := &v
+	p2.X = 420
+	fmt.Println(v)
+}
---
+++ snippets/struct_sp/struct_sp.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Vertex 向量
+type Vertex struct {
+	X    int
+	Y    int
+	Name string
+}
+
+func main() {
+	v := Vertex{1, 2, "a"}
+	fmt.Println(v)
+	v.X = 10
+	fmt.Println(v)
+	fmt.Println(v.X)
+}
