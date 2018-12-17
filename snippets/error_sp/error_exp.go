b02239273434c58977ba985f9503e22b187585cb
clip <binderclip2014@gmail.com>
2018-12-17 20:29:09 +0800

    error

---
+++ snippets/error_sp/error_exp.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	// 打印的时候会寻找 Error()，从而直接用 e 会导致异常
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	return math.Sqrt(x), nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+
+	fmt.Println(ErrNegativeSqrt(-2)) // 会发现优先调用 Error() 来生成 string
+}
