5a5152b3bd8c91ab48369207e8bfac2a453689e0
clip <binderclip2014@gmail.com>
2019-02-27 20:36:39 +0800

    update

--- prjs/gowiki/wiki.go
+++ prjs/gowiki/wiki.go
@@ -89,6 +89,17 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandleFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	// p1.save()
