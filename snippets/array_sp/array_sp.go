243765bdff5e47293babc62e4dcfb4ad54da7160
clip <binderclip2014@gmail.com>
2019-05-06 23:20:05 +0800

    update array sp

--- snippets/array_sp/array_sp.go
+++ snippets/array_sp/array_sp.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var b [3]string
+	b[1] = "ho"
+	fmt.Println(b)
+
 	var a [2]string
 	a[0] = "Hello"
 	a[1] = "World"
@@ -11,6 +15,11 @@ func main() {
 
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+	fmt.Println(len(primes))
+
+	nums := []int{1, 3, 5}
+	fmt.Println(nums)
+	fmt.Println(len(nums))
 
 	// fmt.Println(primes[7]) // invalid array index 7 (out of bounds for 6-element array)
 
