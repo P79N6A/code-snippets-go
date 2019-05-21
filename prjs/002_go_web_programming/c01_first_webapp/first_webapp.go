665bb26071cde1ae0dbb271af96bb34056b1e759
zhoubokun <zhoubokun@bytedance.com>
2019-05-21 14:39:56 +0800

    update web
    Change-Id: I640175489326a12e6f0414fea1d435d21ff89e63

--- prjs/002_go_web_programming/c01_first_webapp/first_webapp.go
+++ prjs/002_go_web_programming/c01_first_webapp/first_webapp.go
@@ -7,9 +7,10 @@ import (
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	fmt.Println(request.RemoteAddr)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
