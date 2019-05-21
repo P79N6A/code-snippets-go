e018990b8c4a948628a3a9e5d42f2c7104262b61
zhoubokun <zhoubokun@bytedance.com>
2019-05-21 16:44:53 +0800

    string
    Change-Id: Ibe0c15c5d1398a411a724744465d8af0f2e32e96

--- snippets/string_sp/string_sp.go
+++ snippets/string_sp/string_sp.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/binderclip/code-snippets-go/utils"
+	"strings"
+)
 
-func main() {
+func string1() {
+	utils.PrintFuncName()
 	fmt.Println("abc", "\141\142\143", "\x61\x62\x63")
 	fmt.Println("\u4f17", "\xe4\xbc\x97", "众")
 
@@ -11,6 +16,17 @@ func main() {
 1 1
 1 2 1`)
 	// 但是 `` 中不能用转义符
+}
+
+func split() {
+	utils.PrintFuncName()
+	s := "ab.c.de."
+	fmt.Println(strings.Split(s, "."))
+}
+
+func main() {
+	string1()
+	split()
 }
 
 // https://gfw.go101.org/article/basic-types-and-value-literals.html
